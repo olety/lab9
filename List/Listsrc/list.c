3286d771213e419e3bc82273df45af77ded82cb1
olety <olety7@gmail.com>
2015-01-11 19:38:45 +0100

    stable v2

--- List/Listsrc/list.c
+++ List/Listsrc/list.c
@@ -10,6 +10,7 @@ List * iniList()
 
 int appendToList(List **head, int val)
 {
+    fprintf(stdout,"appending %d\n",val);
     List *elem=calloc(sizeof(List),1);
     if (elem==0)
         return(0);
@@ -20,7 +21,8 @@ int appendToList(List **head, int val)
 }
 int insertIntoList(List **head, int val, int where)
 {
-    if ( CountListElements(*head) < where )
+    fprintf(stdout,"inserting %d in %dth place\n",val,where);
+    if ( CountListElements(*head) < where || where < 0)
         return 0;
     List *elem=calloc(sizeof(List),1),*left,*right; //left and right can be not alloced because they'll point to existing list chains
     if ( elem == 0 )
@@ -36,23 +38,27 @@ int insertIntoList(List **head, int val, int where)
         return 1;
     }
 
-    for (i; i < where; i++)
+    while(i!= where)
     {
+        i++;
         left = right;
         right = right->next;
     }
 
     left->next = elem;
     elem->next = right;
+
     return (1);
 }
 
 int deleteFromList(List **head, int where)
 {
-    if ( CountListElements(*head) < where )
+    fprintf(stdout,"deleting from %dth place\n",where);
+    if ( CountListElements(*head) < where || where < 0)
         return 0;
     List *temp,*prev;
     temp = *head;
+
     if( where == 0)
     {
         *head = (*(head))->next;
@@ -60,8 +66,9 @@ int deleteFromList(List **head, int where)
         return 1;
     }
     int i = 0;
-    for ( i ; i < where ; i++)
+    while ( i!= where)
     {
+        i++;
         prev = temp;
         temp = temp->next;
     }
@@ -111,27 +118,37 @@ void testList()
 {
     List *head, *p;
     head = iniList();
-    printList(head,5);
+    printList(head,15);
+    //appending
     appendToList(&head, 5);
-    printList(head,5);
+    printList(head,15);
     appendToList(&head, 4);
-    printList(head,5);
+    printList(head,15);
     appendToList(&head, 7);
-    printList(head,5);
+    printList(head,15);
     appendToList(&head, 17);
-    printList(head,5);
+    printList(head,15);
+    //get elems
     p=getElement(head,0);
     p=getElement(head,-1);
     p=getElement(head,2);
     p=getElement(head,10);
+    //inserting
+    insertIntoList(&head, 1, 1);
+    printList(head,15);
+    insertIntoList(&head, 2, 2);
+    printList(head,15);
+    insertIntoList(&head, 0, 0);
+    printList(head,15);
+    //deleting
     deleteFromList(&head, 0);
-    printList(head,5);
+    printList(head,15);
     deleteFromList(&head, 2);
-    printList(head,5);
+    printList(head,15);
     deleteFromList(&head, 1);
-    printList(head,5);
-    deleteFromList(&head, 10);
-    printList(head,5);
+    printList(head,15);
+    deleteFromList(&head, 115);
+    printList(head,15);
     p=getElement(head,0);
     printf("%d\n", p->val);
 }
